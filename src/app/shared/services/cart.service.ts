a7213c616bec98e9ce6149783c88a89f096a59c3
jeevescrypto <jeevan@cryptocrew.com>
2022-12-20 07:25:38 +1100

    local storage

--- src/app/shared/services/cart.service.ts
+++ src/app/shared/services/cart.service.ts
@@ -9,17 +9,30 @@ import { Cart } from '../models/cart';
 })
 export class CartService {
 
+   private readonly cartItemsKey: string = "CART_ITEMS";
+   //This should be set a subject with components listening for updates in multiple tabs.
    private cartItemIds = new Set<number>();
-  constructor(private httpClient:HttpClient) { }
+   constructor(private httpClient:HttpClient) {
+      let storedItems = localStorage.getItem(this.cartItemsKey);
+      if(storedItems && storedItems.length)
+        this.cartItemIds = new Set(JSON.parse(storedItems));
+    }
+
+    updateCartItems()
+    {
+      localStorage.setItem(this.cartItemsKey, JSON.stringify(Array.from(this.cartItemIds)));
+    }
 
     addToCart(productId: number)
     {
       this.cartItemIds.add(productId);
+      this.updateCartItems();
     }
 
     removeFromCart(productId: number)
     {
       this.cartItemIds.delete(productId);
+      this.updateCartItems();
     }
 
     isProductInCart(productId: number): boolean
